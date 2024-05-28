903de8db2163dbd0cabf65668fa2dad869c9bd80
式部 <2112952452@qq.com>
2024-05-28 17:32:46 +0800

    feat: finger

--- src/common/finger/index.ts
+++ src/common/finger/index.ts
@@ -1,26 +1,18 @@
 import { addFeedback } from '../video';
 
-function getBrowserFingerprint() {
-  const navigatorInfo = window.navigator;
-  const screenInfo = window.screen;
-  return {
-    userAgent: navigatorInfo.userAgent, // 浏览器的用户代理字符串
-    language: navigatorInfo.language, // 浏览器首选语言
-    platform: navigatorInfo.platform, // 浏览器平台
-    screenWidth: screenInfo.width, // 屏幕宽度
-    screenHeight: screenInfo.height, // 屏幕高度
-    colorDepth: screenInfo.colorDepth, // 屏幕颜色深度
-    timezoneOffset: new Date().getTimezoneOffset(), // 本地时间与UTC时间的差值，以分钟计
-    sessionStorage: !!window.sessionStorage, // 是否支持sessionStorage
-    localStorage: !!window.localStorage, // 是否支持localStorage
-    indexedDB: !!window.indexedDB, // 是否支持IndexedDB
-    cookiesEnabled: navigatorInfo.cookieEnabled, // 是否启用了cookies
-  };
-}
-
 export function statistics() {
   if (localStorage.getItem('fp') === null) {
-    localStorage.setItem('fp', JSON.stringify(getBrowserFingerprint()));
-    addFeedback(JSON.stringify(getBrowserFingerprint()), 'fp@erisu.moe');
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const fpPromise = import('https://openfpcdn.io/fingerprintjs/v4').then((FingerprintJS) => FingerprintJS.load());
+
+    fpPromise
+      .then((fp) => fp.get())
+      .then((result) => {
+        const { visitorId } = result;
+
+        localStorage.setItem('fp', visitorId);
+        addFeedback(visitorId, 'fp@erisu.moe');
+      });
   }
 }
